3e5f853afda3f0cabb2e73b5ff26594f62d482b2
Russell Heimlich <kingkool68@gmail.com>
2017-12-05 21:07:40 -0500

    Comment out an external domain for now.

--- photon/config.php
+++ photon/config.php
@@ -2,6 +2,8 @@
 // Don't upscale images higher than 3072px
 define( 'UPSCALE_MAX_PIXELS', 3072 );
 
+/*
 add_filter( 'external_fallback_domain', function() {
   return 'https://turbo.russellheimlich.com/';
 } );
+*/
